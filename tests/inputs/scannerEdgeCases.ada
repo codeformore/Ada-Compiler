fb42135ecf5119088c200ebe900f4491b668bda8
Caden Fischer <cadenfischer@gmail.com>
2025-02-15 19:38:47 -0600

    Add tests

--- tests/inputs/scannerEdgeCases.ada
+++ tests/inputs/scannerEdgeCases.ada
@@ -1,7 +1,8 @@
-float. --Only spaces after keywords (in this case float is an identifier)
-abcdefghijklmnop --Length 16 identifier
-abcdefghijklmnopq --Length 17 identifier
-abcdefghijklmnopqr --Length 18 identifier
-3. --No decimal part
-"" --Empty Literal
+float. --Float keyword followed by period
+abcdefghijklmnop --Length 16 identifier Good
+abcdefghijklmnopq --Length 17 identifier Good
+abcdefghijklmnopqr --Length 18 identifier Not Good
+3. --Not legal 
+.5 --Legal period and numt
+"" --Empty Literal Good
 --
---
+++ tests/testScanner.cpp
@@ -0,0 +1,146 @@
+#include <Scanner.hpp>
+#include <gtest/gtest.h>
+
+TEST(ScannerE2E, tokenCoverage)
+{
+    Scanner scanner("tests/inputs/scannerTokenCoverage.ada");
+    for (int tok = 0; tok <= 41; tok++)
+    {
+        scanner.GetNextToken();
+        // std::cout << tok << ": " << scanner.Lexeme << std::endl;
+
+        //Check token attributes if there are any
+        if ((TokenT)tok == numt)
+        {
+            ASSERT_FLOAT_EQ(scanner.ValueR, 3.25f);
+            ASSERT_EQ(scanner.Value, 3);
+        }
+        else if ((TokenT)tok == strt)
+        {
+            ASSERT_STREQ(scanner.Literal.c_str(), "Hello World!");
+        }
+
+        if (tok >= relopt)
+        {
+            if (tok <= 30)
+            {
+                ASSERT_EQ(scanner.Token, relopt);
+            }
+            else if (tok <= 33)
+            {
+                ASSERT_EQ(scanner.Token, addopt);
+            }
+            else if (tok <= 38)
+            {
+                ASSERT_EQ(scanner.Token, mulopt);
+            }
+            else
+            {
+                ASSERT_EQ(scanner.Token, (TokenT)(tok-11));
+            }
+        }
+        else
+        {
+            ASSERT_EQ(scanner.Token, (TokenT)tok);
+        }
+    }
+}
+
+TEST(ScannerE2E, fakeInputFile)
+{
+    const std::string FAKE_FILE_PATH = "NotReal";
+    const std::string ERROR_FAKE_FILE = "Unable to open " + FAKE_FILE_PATH + ".";
+    EXPECT_THROW({
+        try
+        {
+            Scanner scannerFail(FAKE_FILE_PATH);
+        }
+        catch( const std::runtime_error& e )
+        {
+            // and this tests that it has the correct message
+            EXPECT_STREQ(ERROR_FAKE_FILE.c_str(), e.what());
+            throw;
+        }
+    }, std::runtime_error);
+}
+
+TEST(ScannerE2E, emptyInputFile)
+{
+    const std::string FILE_PATH = "tests/inputs/empty.ada";
+    const std::string ERROR_STRING = FILE_PATH + " is empty.";
+    EXPECT_THROW({
+        try
+        {
+            Scanner scannerFail(FILE_PATH);
+        }
+        catch( const std::runtime_error& e )
+        {
+            // and this tests that it has the correct message
+            EXPECT_STREQ(ERROR_STRING.c_str(), e.what());
+            throw;
+        }
+    }, std::runtime_error);
+}
+
+TEST(ScannerE2E, edgeCases)
+{
+    const std::string FILE_PATH = "tests/inputs/scannerEdgeCases.ada";
+    Scanner scanner(FILE_PATH);
+
+    scanner.GetNextToken(); //Get float
+    EXPECT_EQ(scanner.Token, floatt);
+
+    scanner.GetNextToken(); //Get period
+    EXPECT_EQ(scanner.Token, periodt);
+
+    scanner.GetNextToken(); //Get length 16 idt
+    EXPECT_EQ(scanner.Token, idt);
+
+    scanner.GetNextToken(); //Get length 17 idt
+    EXPECT_EQ(scanner.Token, idt);
+
+    EXPECT_THROW({ //Expect length 18 exception
+        try
+        {
+            scanner.GetNextToken();
+        }
+        catch( const std::runtime_error& e )
+        {
+            // and this tests that it has the correct message
+            EXPECT_STREQ("Identifier abcdefghijklmnopqr too long", e.what());
+            throw;
+        }
+    }, std::runtime_error);
+
+    EXPECT_THROW({ //Expect invalid float exception
+        try
+        {
+            scanner.GetNextToken();
+        }
+        catch( const std::runtime_error& e )
+        {
+            // and this tests that it has the correct message
+            EXPECT_STREQ("Number with period not followed by decimal part.", e.what());
+            throw;
+        }
+    }, std::runtime_error);
+
+    scanner.GetNextToken(); //Get period token
+    EXPECT_EQ(scanner.Token, periodt);
+
+    scanner.GetNextToken(); //Get number token
+    EXPECT_EQ(scanner.Token, numt);
+    EXPECT_EQ(scanner.Value, 5);
+
+    scanner.GetNextToken(); //Get the empty literal
+    EXPECT_EQ(scanner.Token, strt);
+    EXPECT_STREQ(scanner.Literal.c_str(), "");
+
+    scanner.GetNextToken(); //Get the eof token
+    EXPECT_EQ(scanner.Token, eoft);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
