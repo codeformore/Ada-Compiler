b30df4e2faa494232ebaa4a9be1fdf7d9891ca2f
Caden Fischer <cadenfischer@gmail.com>
2025-03-04 16:25:21 -0600

    Implement hash for SymTbl

---
+++ src/SymTbl.cpp
@@ -0,0 +1,67 @@
+/********************************************************************
+*** NAME : Caden Fischer ***
+*** CLASS : CSC 446 ***
+*** INSTRUCTOR : Dr. George Hamer ***
+*********************************************************************
+*** FILE: Scanner.hpp ***
+*** DESCRIPTION : A scanner for the Ada language subset. ***
+*** EXPORTED FUNCTIONALITY : 
+* enum EntryType
+* enum VarType
+* enum ConstType
+* enum ParamMode
+* struct Param
+*   VarType type
+*   ParamMode mode
+*   Param* next
+* struct VarEntry
+*   VarType type
+*   int offset
+*   int size
+* struct ConstEntry
+*   ConstType type
+*   union { int value; float valueR; }
+* struct ProcEntry
+*   int size
+*   int numParams
+*   Param* params
+* struct SymTblEntry
+*   TokenT token
+*   std::string lexeme
+*   int depth
+*   SymTblEntry* next
+*   EntryType entryType
+*   union { VarEntry variable; ConstEntry constant; ProcEntry procedure; }
+* class SymTbl
+*   void Insert(std::string lex, TokenT token, int depth)
+*   SymTblEntry* Lookup(std::string lex)
+*   void DeleteDepth(int depth)
+*   void WriteTable(int depth)
+********************************************************************/
+
+#include <SymTbl.hpp>
+#include <cstring>
+
+//hashpjw from P. J. Weinberger's C compiler
+//Some Test Values: 
+//  "hi" -> 4
+//  "hello" -> 202
+int SymTbl::hash(std::string lex)
+{
+    char *s;
+    s = new char[lex.length()];
+    strncpy(s, lex.c_str(), lex.length());
+    int h = 0, g;
+    for (int i = 0; i < (int)lex.length(); i++)
+    {
+        h = (h << 24) + s[i];
+        g = h & 0xf0000000;
+        if (g != 0)
+        {
+            h = h ^ (g >> 24);
+            h = h ^ g;
+        }
+    }
+    return h % TBL_SIZE;
+}
+
