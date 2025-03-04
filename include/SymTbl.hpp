e7707da9224582f4e2306d0cf4a7cc0c2467e42e
Caden Fischer <cadenfischer@gmail.com>
2025-03-04 16:23:58 -0600

    Create header file for SymTbl

---
+++ include/SymTbl.hpp
@@ -0,0 +1,121 @@
+/********************************************************************
+*** NAME : Caden Fischer ***
+*** CLASS : CSC 446 ***
+*** INSTRUCTOR : Dr. George Hamer ***
+*********************************************************************
+*** FILE: Scanner.hpp ***
+*** DESCRIPTION : A scanner for the Ada language subset. ***
+*** EXPORTED FUNCTIONALITY : 
+* enum EntryType
+* enum VarType
+* enum ConstType
+* enum ParamMode
+* struct Param
+*   VarType type
+*   ParamMode mode
+*   Param* next
+* struct VarEntry
+*   VarType type
+*   int offset
+*   int size
+* struct ConstEntry
+*   ConstType type
+*   union { int value; float valueR; }
+* struct ProcEntry
+*   int size
+*   int numParams
+*   Param* params
+* struct SymTblEntry
+*   TokenT token
+*   std::string lexeme
+*   int depth
+*   SymTblEntry* next
+*   EntryType entryType
+*   union { VarEntry variable; ConstEntry constant; ProcEntry procedure; }
+* class SymTbl
+*   void Insert(std::string lex, TokenT token, int depth)
+*   SymTblEntry* Lookup(std::string lex)
+*   void DeleteDepth(int depth)
+*   void WriteTable(int depth)
+********************************************************************/
+
+#ifndef SYMTBL_HPP
+#define SYMTBL_HPP
+
+#include <Scanner.hpp>
+#include <string>
+
+const int TBL_SIZE = 211;
+
+enum EntryType { Variable, Constant, Procedure };
+
+enum VarType { IntVar, RealVar, CharVar };
+
+enum ConstType { IntConst, RealConst };
+
+enum ParamMode { InMode, OutMode, InoutMode };
+
+struct Param
+{
+    VarType type;
+    ParamMode mode;
+    Param* next;
+};
+
+struct VarEntry
+{
+    VarType type;
+    int offset;
+    int size;
+};
+
+struct ConstEntry
+{
+    ConstType type;
+    union
+    {
+        int value;
+        float valueR;
+    };
+};
+
+struct ProcEntry
+{
+    int size;
+    int numParams;
+    Param* params;
+    void AddParam(VarType type, ParamMode mode);
+    ~ProcEntry();
+};
+
+struct SymTblEntry
+{
+    TokenT token;
+    std::string lexeme;
+    int depth;
+    SymTblEntry* next;
+    EntryType entryType;
+    union
+    {
+        VarEntry variable;
+        ConstEntry constant;
+        ProcEntry procedure;
+    };
+    ~SymTblEntry() { printf("SymTblEntry Destructor\n"); };
+};
+
+class SymTbl
+{
+private:
+    SymTblEntry* entries[TBL_SIZE];
+    int hash(std::string lex);
+public:
+    void Insert(std::string lex, TokenT token, int depth);
+    SymTblEntry* Lookup(std::string lex);
+    void DeleteDepth(int depth);
+    void WriteTable(int depth);
+    SymTbl();
+    ~SymTbl();
+};
+
+#endif
