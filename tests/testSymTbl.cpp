63ca9f9ec12e9e90d12b177e31362770c0eaa6e9
Caden Fischer <cadenfischer@gmail.com>
2025-03-06 16:41:00 -0600

    Add tests for SymTbl

--- tests/testSymTbl.cpp
+++ tests/testSymTbl.cpp
@@ -1,28 +1,57 @@
 #include <SymTbl.hpp>
 #include <gtest/gtest.h>
 
-TEST(SymTblTest, Destructors)
+//A version of the SymTbl class to test the hash function
+class TestableSymTbl : public SymTbl {
+public:
+    using SymTbl::hash;  // Expose for testing
+};
+
+//Create test fixture
+class SymTblTest : public ::testing::Test {
+protected:
+    TestableSymTbl symTbl;  // Test fixture's member
+
+    // Optional setup function (runs before each test)
+    void SetUp() override {
+        SymTblEntry* cur;
+        symTbl.Insert("first", idt, 1);
+        cur = symTbl.Lookup("first");
+        cur->entryType = Variable;
+        symTbl.Insert("second", idt, 1);
+        cur = symTbl.Lookup("second");
+        cur->entryType = Constant;
+        symTbl.Insert("first", idt, 2);
+        cur = symTbl.Lookup("first");
+        cur->entryType = Procedure;
+        cur->procedure.AddParam(CharVar, InoutMode);
+        cur->procedure.AddParam(IntVar, InMode);
+        cur->procedure.numParams = 2;
+        cur->procedure.size = 4;
+    }
+
+    // void TearDown() override {
+    //     symTbl = TestableSymTbl();
+    // }
+};
+
+TEST_F(SymTblTest, testHash)
 {
-    SymTbl symTbl;
-    symTbl.Insert("test", idt, 1);
-    symTbl.Insert("anotherTest", idt, 1);
-    symTbl.Insert("anotherTest", idt, 1);
-    symTbl.WriteTable(1);
-    SymTblEntry* test = symTbl.Lookup("anotherTest");
-    test->entryType = Procedure;
-    test->procedure.numParams = 1;
-    test->procedure.size = 4;
-    test->procedure.params = new Param;
-    test->procedure.params->mode = InMode;
-    test->procedure.params->next = nullptr;
-    test->procedure.params->type = RealVar;
-    Param* another = new Param;
-    another->mode = OutMode;
-    another->next = test->procedure.params;
-    another->type = CharVar;
-    test->procedure.params = another;
-    symTbl.DeleteDepth(1);
-    symTbl.WriteTable(1);
+    EXPECT_EQ(symTbl.hash("hi"), 4);
+    EXPECT_EQ(symTbl.hash("hello"), 209); //is this correct?
+}
+
+TEST_F(SymTblTest, testLookup)
+{
+    SymTblEntry* cur = symTbl.Lookup("first");
+    EXPECT_STREQ(cur->lexeme.c_str(), "first");
+    EXPECT_EQ(cur->depth, 2);
+    EXPECT_EQ(cur->entryType, Procedure);
+    EXPECT_EQ(cur->token, idt);
+    EXPECT_EQ(cur->procedure.params->mode, InMode);
+    EXPECT_EQ(cur->procedure.params->type, IntVar);
+    EXPECT_EQ(cur->procedure.params->next->mode, InoutMode);
+    EXPECT_EQ(cur->procedure.params->next->type, CharVar); 
 }
 
 int main(int argc, char **argv) {
